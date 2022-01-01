76fa96ef670a99ba74b5fd655b077e42050d9160
Amr Hammoud <amrhammoud.dev@gmail.com>
2022-01-01 19:38:08 +0200

    6

--- Data Insertion.sql
+++ Data Insertion.sql
@@ -2,7 +2,7 @@
 --RESIDENTS
 --INSERT INTO residents VALUES('ssn','fname','mname','lname','city','street','apartment','bdate','num','gender');
 
--- r1 INSERT INTO residents VALUES('1510004587','Michel','Elias','Farah','city','street','apartment','bdate','num','gender');
+INSERT INTO residents VALUES('1510004587','Michel','Elias','Farah','city','street','apartment','bdate','num','gender');
 -- r2 INSERT INTO residents VALUES('1510002731','George','Ghassan','Nkoul','Halba','Aabdeh','98','1993-08-13','70758831','M');
 -- r3 INSERT INTO residents VALUES('1520000168','kamil','Sleiman','Nehmeh','Halba','Cheikh Mohammad','26','1961-01-02','71927478','M');
 -- r4 INSERT INTO residents VALUES('1520000169','Caroline','Riyad','Saud','Halba','Cheikh Mohammad','19','1984-10-23','71937838','F');
